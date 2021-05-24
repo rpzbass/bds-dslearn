de56b1939fbc21801ed1850ea999664bb079e85f
rpźbass <ronaldop.tech@gmail.com>
2021-05-24 17:10:34 -0300

    Add Course and Insertion in the data.sql

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -9,4 +9,8 @@ INSERT INTO tb_role (authority) VALUES ('ROLE_ADMIN');
 INSERT INTO tb_user_role (user_id, role_id) VALUES (1, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 1);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (2, 2);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 1);
+INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 2);
 INSERT INTO tb_user_role (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO tb_course (name, img_Uri, img_Gray_Uri) VALUES('BootCamp  HTML', 'https://cdn.slidesharecdn.com/ss_thumbnails/htmlcss-161121095145-thumbnail-4.jpg', 'https://www.ecollege.ie/wp-content/uploads/2020/02/Course_Featured_Security.jpg');
