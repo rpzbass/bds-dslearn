d122c907790966c731b51dfab75c2459dfb79017
rpźbass <ronaldop.tech@gmail.com>
2021-05-24 16:13:22 -0300

    Add Class User e Role

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/Dslearn1Application.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dslearnbds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dslearn1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dslearn1Application.class, args);
+	}
+
+}
